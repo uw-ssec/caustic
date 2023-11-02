e6c64ce04e1f70a10f393c40f9b8b39a82f6972d
uwcdc <127983572+uwcdc@users.noreply.github.com>
2023-11-02 15:49:42 -0700

    Move project to src/ directory

---
+++ src/caustics/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.5.0'
+__author__ = "Ciela"
+
+from .constants import *
+from .lenses import *
+from .cosmology import *
+from .packed import *
+from .parametrized import *
+from .light import *
+from .utils import *
+from .sims import *
+# from .demo import *
