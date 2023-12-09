f99878594fa1a841ed244bd3af456488de069a95
Landung 'Don' Setiawan <landungs@uw.edu>
2023-12-08 17:10:18 -0800

    style: Expand imports for data module

--- src/caustics/data/__init__.py
+++ src/caustics/data/__init__.py
@@ -1,3 +1,5 @@
-from . import hdf5dataset, illustris_kappa, probes
+from .hdf5dataset import HDF5Dataset
+from .illustris_kappa import IllustrisKappaDataset
+from .probes import PROBESDataset
 
-__all__ = ["hdf5dataset", "illustris_kappa", "probes"]
+__all__ = ["HDF5Dataset", "IllustrisKappaDataset", "PROBESDataset"]
