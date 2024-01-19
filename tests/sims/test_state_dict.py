27b430f58b40cea175096bf45353c4110eac9d2d
Landung 'Don' Setiawan <landungs@uw.edu>
2024-01-19 08:44:08 -0800

    test: Fix state dict comparison with empty torch
    Fixed the comparison for StateDicts. Since comparing
    empty tensors doesn't work, I created isEquals internal
    function to the 'test_from_params' test to compare
    values one by one.

--- tests/sims/test_state_dict.py
+++ tests/sims/test_state_dict.py
@@ -5,7 +5,7 @@ from safetensors.torch import save, load
 from datetime import datetime as dt
 from caustics.parameter import Parameter
 from caustics.namespace_dict import NamespaceDict, NestedNamespaceDict
-from caustics.sims.state_dict import StateDict, IMMUTABLE_ERR
+from caustics.sims.state_dict import StateDict, IMMUTABLE_ERR, _sanitize
 from caustics import __version__
 
 
@@ -43,21 +43,52 @@ class TestStateDict:
     def test_from_params(self, simple_common_sim):
         params: NestedNamespaceDict = simple_common_sim.params
 
+        # Extract the "static" and "dynamic" parameters
+        param_dicts = list(params.values())
+
+        # Extract the "static" and "dynamic" parameters
+        # to a single merged dictionary
+        final_dict = NestedNamespaceDict()
+        for pdict in param_dicts:
+            for k, v in pdict.items():
+                if k not in final_dict:
+                    final_dict[k] = v
+                else:
+                    final_dict[k] = {**final_dict[k], **v}
+
         # flatten function only exists for NestedNamespaceDict
-        static_params: NamespaceDict = params["static"].flatten()
-        tensors_dict: Dict[str, torch.Tensor] = {
-            k: v.value for k, v in static_params.items()
-        }
+        all_params = final_dict.flatten()
+
+        tensors_dict: Dict[str, torch.Tensor] = _sanitize(
+            {k: v.value for k, v in all_params.items()}
+        )
 
         expected_state_dict = StateDict(tensors_dict)
 
+        def isEquals(a, b):
+            # Go through each key and values
+            # change empty torch to be None
+            # since we can't directly compare
+            # empty torch
+            truthy = []
+            for k, v in a.items():
+                if k not in b:
+                    return False
+                kv = b[k]
+                if (v.nelement() == 0) or (kv.nelement() == 0):
+                    v = None
+                    kv = None
+                truthy.append(v == kv)
+
+            return all(truthy)
+
         # Full parameters
         state_dict = StateDict.from_params(params)
-        assert state_dict == expected_state_dict
+        assert isEquals(state_dict, expected_state_dict)
 
         # Static only
-        state_dict = StateDict.from_params(static_params)
-        assert state_dict == expected_state_dict
+        state_dict = StateDict.from_params(all_params)
+        assert isEquals(state_dict, expected_state_dict)
 
     def test_to_params(self, simple_state_dict):
         params = simple_state_dict.to_params()
