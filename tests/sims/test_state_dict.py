97bc22b83b4d42d0815d4145629b06cf4b1d5055
Landung 'Don' Setiawan <landungs@uw.edu>
2024-01-23 15:26:13 -0800

    refactor: Use Path.cwd instead of os.path.curdir

--- tests/sims/test_state_dict.py
+++ tests/sims/test_state_dict.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
-import os
 import sys
 
 import pytest
@@ -138,7 +137,7 @@ class TestStateDict:
 
     def test_save(self, simple_state_dict):
         # Check for default save path
-        expected_fpath = Path(os.path.curdir) / simple_state_dict._StateDict__st_file
+        expected_fpath = Path.cwd() / simple_state_dict._StateDict__st_file
         default_fpath = simple_state_dict.save()
 
         assert Path(default_fpath).exists()
